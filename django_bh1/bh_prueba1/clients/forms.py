34a63327299efcc8525d3ea903db1a81482b3856
Alexei Horta Moiseevich <ahorta@palconsulting.local>
2018-09-13 17:54:33 -0500

    prueba desde el trabajo

---
+++ django_bh1/bh_prueba1/clients/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Company
+
+class CompanyForm(forms.ModelForm):
+    class Meta:
+        model = Company
+        fields = ['name', 'address', 'second_adress', 'city', 'rfc', 'phone', 'mobile', 'email', 'website']
+        widgets = {
+            'name': forms.Textarea(attrs={'class':'validate'}),
+            'address': forms.Textarea(attrs={'class':'validate', 'rows':3}),
+            'second_adress': forms.Textarea(attrs={'class':'validate', 'rows':3}),
+            'city': forms.Textarea(attrs={'class':'validate'}),
+            'rfc': forms.Textarea(attrs={'class':'validate'}),
+            'mobile': forms.Textarea(attrs={'class':'validate'}),
+            'email': forms.EmailField(attrs={'class':'validate'}),
+            'website': forms.URLInput(attrs={'class':'validate'}),
+        }
